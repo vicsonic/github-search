dd262ea1d1ff2a3ef65ce41dada03db3fc1b3a9d
vicsonic <vicsonic@gmail.com>
2023-03-08 22:33:25 -0600

    Add simple loading

---
+++ GitHubSearch/Modules/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  GitHubSearch
+//
+//  Created by Victor Erik Soto Martinez on 08/03/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
